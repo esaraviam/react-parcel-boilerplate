2acd12130715f4017f2968394a1494a5c550f31b
Esteban Saravia <estebansaravia@gmail.com>
2017-12-28 12:09:13 -0300

    cleaning the house

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import './main.css'
+import Layout from './Layout'
 
 class Main extends Component{
+    handleClick(){
+        console.log("Hola click")
+    }
 
     render(){       
         
         return (
             <div className="media">
                 <h1>HELLO WORLD FROM PARCEL 2 </h1>
+                <Layout mensaje="SOY UN MENSAJE" handleClick={this.handleClick}/>
             </div>
         )
     }
